be4f64f77581e173813a0e8406008e9b5200524c
Anton Kozlov <tonickkozlov@me.com>
2017-03-15 20:40:51 +0300

    [feat] Added TweetList page

---
+++ src/components/TweetList.vue
@@ -0,0 +1,17 @@
+<!-- Will list tweets received from given url -->
+<template>
+    <div class="tweet-list">
+        <Tweet v-for="id in tweetIds" :id="id" :key="id"></Tweet>
+    </div>
+</template>
+
+<script>
+import Tweet from 'vue-tweet-embed'
+
+export default {
+    props: ['tweetIds'],
+    components: {
+        Tweet
+    }
+}
+</script>
