2a6274a759e15e1c8283c4579e6dc63084dc1f3e
Anton Kozlov <tonickkozlov@me.com>
2017-03-18 21:32:09 +0300

    [feat] Added placeholder tweets while real ones are being loaded

--- src/components/TweetList.vue
+++ src/components/TweetList.vue
@@ -1,17 +1,26 @@
 <!-- Will list tweets received from given url -->
 <template>
     <div class="tweet-list">
-        <Tweet v-for="id in tweetIds" :id="id" :key="id"></Tweet>
+        <Tweet v-for="id in tweetIds" :id="id" :key="id"><TweetPlaceholder /></Tweet>
     </div>
 </template>
 
 <script>
 import Tweet from 'vue-tweet-embed'
+import TweetPlaceholder from '@/components/TweetPlaceholder'
 
 export default {
     props: ['tweetIds'],
     components: {
-        Tweet
+        Tweet,
+        TweetPlaceholder
     }
 }
 </script>
+
+<style scoped>
+.tweet-list {
+    height: 100%;
+    overflow-y: scroll;
+}
+</style>
---
+++ src/components/TweetPlaceholder.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="tweet-placeholder"><Loader color="lightblue" /></div>
+</template>
+
+<script>
+import Loader from 'vue-spinner/src/ScaleLoader.vue'
+export default {
+    components: {
+        Loader
+    }
+}
+</script>
+
+<style scoped>
+.tweet-placeholder {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 140px;
+    border: 1px solid rgb(225, 232, 237);
+    border-radius: 4px;
+    max-width: 100%;
+    width: 500px;
+    min-width: 220px;
+    margin-top: 10px;
+    margin-bottom: 10px
+}
+</style>
